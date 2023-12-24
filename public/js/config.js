cc04f181482ebd1b7cbdcc94bd201b442ea4b133
AnaSiles <anasiles_232@yahoo.com>
2023-12-24 11:13:25 +0100

    Modifico requiere y module.export por import para solucionar require is not defined

--- public/js/config.js
+++ public/js/config.js
@@ -1,5 +1,9 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const host = process.env.HOST;
+// const dotenv = require("dotenv");
+// dotenv.config();
+// const host = process.env.HOST;
 
-module.exports = host;
+// module.exports = host;
+
+import { config } from "dotenv";
+config();
+export const host = process.env.HOST;
